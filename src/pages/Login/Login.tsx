41c50d360905e4a454491d37cdedd32fa142dcfe
khoitran3172 <khoi.tran3172@gmail.com>
2023-10-06 22:31:17 +0700

    Login-Change

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,11 +1,45 @@
-import { Box } from '@mantine/core';
+
+import { TextInput, Checkbox, Code, Text, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
 function MonitorPage() {
+ 
+   const form = useForm({
+    initialValues: {
+      terms: false,
+      user: {
+        Email: '',
+        Password: '',
+      },
+    },
+  });
 
   return (
-    <Box className='h-full'>
-      <div>Login</div>
+    <Box  maw={340} mx="auto">
+      <TextInput
+        label="Email"
+        placeholder="Email"
+        {...form.getInputProps('user.firstName')}
+      />
+      <TextInput
+        label="Password"
+        placeholder="Password"
+        mt="md"
+        {...form.getInputProps('user.lastName')}
+      />
+      <Checkbox
+        label="I accepts terms & conditions"
+        mt="sm"
+        {...form.getInputProps('terms', { type: 'checkbox' })}
+      />
+
+      {/* <Text size="sm" fw={500} mt="xl">
+        Form values:
+      </Text>
+      <Code block mt={5}>
+        {JSON.stringify(form.values, null, 2)}
+      </Code> */}
     </Box>
   );
+ 
 }
-
 export default MonitorPage;
