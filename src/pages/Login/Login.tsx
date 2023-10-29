8376d19857ba6bca27c377bfdda9f6d5498c5c5b
khoitran3172 <khoi.tran3172@gmail.com>
2023-10-29 21:52:13 +0700

    login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,78 +1,69 @@
 
-import { TextInput, Checkbox, Code, Text, Box, Grid, Tabs, rem, Button } from '@mantine/core';
+import { TextInput, Image , Checkbox, Code, Text, Box, Grid, Tabs, rem, Button } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconPhoto, IconMessageCircle, IconSettings } from '@tabler/icons-react';
 import { Link } from 'react-router-dom';
 function MonitorPage() {
   const iconStyle = { width: rem(12), height: rem(12) };
-   const form = useForm({
+  const form = useForm({
     initialValues: {
-      terms: false,
-      user: {
-        Email: '',
-        Password: '',
-      },
+      email: '',
+      termsOfService: false,
     },
+
+    validate: {
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+    },
+    
   });
 
   return (
-    <div className='w-screen'>
-    <Box  maw='80%' mx="auto">
+    <div className='w-screen h-screen' >
+    <Box  className='h-full'>
     <Grid>
-      <Grid.Col span={6}>1</Grid.Col>
-      <Grid.Col span={5}>
-        <div><Tabs defaultValue="gallery">
-      <Tabs.List>
-        <Tabs.Tab value="gallery">
-          <Button><Link to="../">trang chu</Link></Button>
-        </Tabs.Tab>
-        <Tabs.Tab value="messages">
-        <Button><Link to="./login">login</Link></Button>
-        </Tabs.Tab>
-        <Tabs.Tab value="settings">
-          Settings
-        </Tabs.Tab>
-      </Tabs.List>
+      {/* Image */}
+      <Grid.Col span={6}>
+     <img src="src/pages/Login/Frame 6.png" alt="" height={900} width={820} />
+        </Grid.Col>
+        {/* Login */}
+        
+      <Grid.Col span={6}>
 
-      <Tabs.Panel value="gallery">
-        Gallery tab content
-      </Tabs.Panel>
-
-      <Tabs.Panel value="messages">
-        Messages tab content
-      </Tabs.Panel>
-
-      <Tabs.Panel value="settings">
-        Settings tab content
-      </Tabs.Panel>
-    </Tabs></div>
-        <div><TextInput
+        <div className='flex justify-center items-center h-full flex-col'>
+        <div className="bg-no-repeat bg-center flex justify-center"  >
+        <img src="src/pages/Login/Frame 7.png" alt="" height={100} width={100} />
+        </div>
+        <div>
+          <h1>LOGIN</h1>
+          </div>
+          <div>
+          <TextInput
         label="Email"
         placeholder="Email"
-        {...form.getInputProps('user.firstName')}
+        description=""
+        inputWrapperOrder={['label', 'error', 'input', 'description']}
       />
       <TextInput
+        mt="xl"
         label="Password"
-        placeholder="Password"
-        mt="md"
-        {...form.getInputProps('user.lastName')}
+        placeholder="Email"
+        description=""
+        // error=""
+        inputWrapperOrder={['label', 'input', 'description', 'error']}
       />
-      <Checkbox
-        label="I accepts terms & conditions"
-        mt="sm"
-        {...form.getInputProps('terms', { type: 'checkbox' })}
-      /></div>
-        
-      </Grid.Col>
-    </Grid>
-      
+    <div className='flex justify-center pt-5'> {/* pt-5 is for padding-top */}
+    <Button fullWidth size="md" variant="filled">Login</Button>
+    </div>
 
-      {/* <Text size="sm" fw={500} mt="xl">
-        Form values:
-      </Text>
-      <Code block mt={5}>
-        {JSON.stringify(form.values, null, 2)}
-      </Code> */}
+    <div className='flex justify-center pt-5'> {/* pt-5 is for padding-top */}
+    <Button fullWidth size="md" variant="filled">Cancel</Button>
+    </div>
+          </div>
+        </div>
+        <div>
+        </div>
+        </Grid.Col>
+    </Grid>
     </Box>
     </div>
   );
